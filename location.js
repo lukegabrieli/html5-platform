74157810eadb452b70cc22c1c01bd29bc637b328
Luke Gabrieli <usetheforce10@gmail.com>
2015-06-10 16:25:54 -0700

    the rest of our files

--- location.js
+++ location.js
@@ -1,11 +1,16 @@
-var map;
 function initialize() {
+  var myLatlng = new google.maps.LatLng(47.615175, -122.318403);
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(47.615175, -122.318403)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    zoom: 14,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'The Stranger'
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
